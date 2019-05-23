03118f6483d09e34f474233eb3ec97ae6b36a51a
Dmitriy Gritsenko <evil1@inbox.ru>
2019-05-23 12:24:04 +0600

    Created empty app with modx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DevTools from 'mobx-react-devtools';
 
 function App() {
   return (
     <div className="App">
+      <DevTools />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
